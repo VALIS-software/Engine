fbb47ca1597d8196ae104fe5c5e93e5cacf645b2
George Corney <haxiomic@gmail.com>
2019-02-18 20:48:58 +0000

    Update build

--- ui/Text.d.ts
+++ ui/Text.d.ts
@@ -17,6 +17,7 @@ export declare class Text extends Object2D {
     strokeEnabled: boolean;
     color: ArrayLike<number>;
     strokeColor: ArrayLike<number>;
+    strokeWidthPx: 1.0;
     opacity: number;
     /**
      * When additive blend factor is 1, the blend mode is additive, when 0, it's normal premultiplied alpha blended
